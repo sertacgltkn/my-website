cb1f9abb77593c33dafd93264bc7aa875a827a18
Sertaç Gültekin <sertacgltkn@gmail.com>
2022-08-30 21:52:00 +0300

    about changed

--- src/components/About.js
+++ src/components/About.js
@@ -1,32 +1,34 @@
 import React from "react";
 import iki from "../assets/iki.jpg";
 import AboutLayout from "../layout/AboutLayout";
-import Typography from '@mui/material/Typography';
-
-
+import Typography from "@mui/material/Typography";
+import { LoremIpsum } from "react-lorem-ipsum";
 
 function About() {
-
-
-
   return (
-
     <AboutLayout
       sxBackground={{
         backgroundImage: `url(${iki})`,
-        backgroundColor: '#7fc7d9', // Average color of the background image.
-        backgroundPosition: 'center',
+        backgroundColor: "#7fc7d9", // Average color of the background image.
+        backgroundPosition: "center",
       }}
     >
       {/* Increase the network loading priority of the background image. */}
-     {/*  <img
+      {/*  <img
         style={{ display: 'none' }}
         src={about1}
         alt="increase priority"
 
       /> */}
-      <Typography mt="auto" color="inherit" align="center" variant="h2" marked="center">
-        Bizi Keşfedin
+      <Typography
+        mt="auto"
+        color="inherit"
+        align="center"
+        variant="h2"
+        marked="center"
+      >
+        <hr/>
+        Lorem
       </Typography>
       <Typography
         color="inherit"
@@ -34,18 +36,13 @@ function About() {
         variant="h5"
         sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}
       >
-        Henüz yolda iken size uygun dönüş yüklerinden haberdar olabilirsiniz.
-        Aracınıza uygun yüklere teklif verebilirsiniz.
-        Yükleme noktasına navigasyon desteği alabilirsiniz.
-        Hiçbir komisyon ücreti ödemeden yükü alabilirsiniz.
-        Taşıma ve yük teslimi sırasında oluşacak tüm srounları tek tıkla bildirebilirsiniz.
-        Carvel üzerinden e-fatura kesebilirsiniz.
+        <LoremIpsum/>
+        <LoremIpsum avgWordsPerSentence={1} />
       </Typography>
       <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
-        Tecrübeyi Deneyimleyin
+        lorem İpsum
       </Typography>
     </AboutLayout>
-  )
-
+  );
 }
 export default About;
