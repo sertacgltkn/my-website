da8420d7d3d04c1ef3dd72924bb7a65441aa5f2d
Sertaç Gültekin <sertacgltkn@gmail.com>
2022-09-12 13:22:02 +0300

    home added for RRdom

--- src/App.js
+++ src/App.js
@@ -4,19 +4,36 @@ import Navbar from "../src/components/Navbar";
 import Media from "../src/components/Media";
 import Footer from "./components/Footer";
 import About from "./components/about/About";
+import Carousel from "./components/Carousel";
+import Alerty from "./components/Alerty";
+import Home from "./components/Home";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <Router>
       <Navbar />
-      <About />
-      <Card />
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+        <Route exact path='/about' element={<About />}/>
+        <Route path='/card' element={<Card />}/>
+        <Route path='/media' element={<Media />}/>
+        <Route path='/carousel' element={<Carousel />}/>
+        <Route path='/alerty' element={<Alerty />}/>
+      </Routes>
       
-      <Media />
-     
       <Footer />
+
       
-      
+      </Router>
     </div>
   );
 }
