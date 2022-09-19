a899a61b2cac2dc17ccde19ed7dc882235764e63
Sertaç Gültekin <sertacgltkn@gmail.com>
2022-09-19 16:34:38 +0300

    some changes

--- src/components/Alerty.js
+++ src/components/Alerty.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Alerty.css"
 
 function Alerty() {
   return (
-    <div >
+    <div className="alerty">
       <br></br><br></br><br></br><br></br><br></br>
       BURAYA BAŞAK BİR COMPONENT EKLENEBİLİR
+      <h4>hellooooo</h4>
     </div>
   );
 }
